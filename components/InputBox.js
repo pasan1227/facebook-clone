560c20da017ef272ef14c2412dd383326224b49a
Pasan Ratnayake <pasanratnayake@gmail.com>
2021-07-07 10:42:10 +0530

    InputBox Component (2/2)

--- components/InputBox.js
+++ components/InputBox.js
@@ -2,12 +2,59 @@ import { CameraIcon, EmojiHappyIcon } from '@heroicons/react/outline';
 import { VideoCameraIcon } from '@heroicons/react/solid';
 import { useSession } from 'next-auth/client';
 import Image from 'next/image';
+import { useRef, useState } from 'react';
+import { db, storage } from '../firebase';
+import firebase from 'firebase';
 
 function InputBox() {
     const [session] = useSession();
+    const inputRef = useRef(null);
+    const filepickerRef = useRef(null);
+    const [imageToPost, setImageToPost] = useState(null);
 
     const sendPost = (e) => {
         e.preventDefault();
+
+        if (!inputRef.current.value) return;
+
+        db.collection("posts").add({
+            message: inputRef.current.value,
+            name: session.user.name,
+            email: session.user.email,
+            image: session.user.image,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        }).then(doc => {
+            if (imageToPost) {
+                const uploadTask = storage.ref(`posts/${doc.id}`).putString(imageToPost, 'data_url');
+
+                removeImage();
+
+                uploadTask.on('state_change', null, error => console.error(error), () => {
+                    // When the upload completes
+                    storage.ref('posts').child(doc.id).getDownloadURL().then(url => {
+                        db.collection('posts').doc(doc.id).set({
+                            postImage: url
+                        }, { merge: true });
+                    });
+                });
+            }
+        });
+
+        inputRef.current.value = "";
+    };
+
+    const addImageToPost = (e) => {
+        const reader = new FileReader();
+        if (e.target.files[0]) {
+            reader.readAsDataURL(e.target.files[0]);
+        }
+        reader.onload = (readerEvent) => {
+            setImageToPost(readerEvent.target.result);
+        }
+    };
+
+    const removeImage = () => {
+        setImageToPost(null);
     };
 
     return (
@@ -22,10 +69,18 @@ function InputBox() {
                     alt=""
                 />
                 <form className="flex flex-1">
-                    <input className="rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none" type="text" placeholder={`What's on your mind, ${session.user.name}?`} 
+                    <input className="rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none" type="text" ref={inputRef} placeholder={`What's on your mind, ${session.user.name}?`} 
                     />
-                    <button hidden type="submit" onClick="sendPost">Submit</button>
+                    <button hidden type="submit" onClick={sendPost}>Submit</button>
                 </form>
+
+                {imageToPost && (
+                    <div onClick={removeImage} className="flex flex-col filter hover:brightness-110 transition duration-150 transform hover:scale-105 cursor-pointer" >
+                        <img className="h-10 object-contain" src={imageToPost} alt="" />
+                        <p className="text-xs text-red-500 text-center">Remove</p>
+                    </div>
+                )}
+
             </div>
 
             <div className="flex justify-evenly p-3 border-t">
@@ -34,9 +89,10 @@ function InputBox() {
                     <p className="text-xs sm:text-sm xl:text-base">Live Video</p>
                 </div>
 
-                <div className="inputIcon">
+                <div onClick={() => filepickerRef.current.click()} className="inputIcon">
                     <CameraIcon className="h-7 text-green-400" />
                     <p className="text-xs sm:text-sm xl:text-base">Photo/Video</p>
+                    <input ref={filepickerRef} onChange={addImageToPost} type="file" hidden />
                 </div>
 
                 <div className="inputIcon">
