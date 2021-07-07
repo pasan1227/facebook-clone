163dc3f4c0f5bfbe7efd98e66902f3493bbdce6a
Pasan Ratnayake <pasanratnayake@gmail.com>
2021-07-07 17:29:20 +0530

    widgets component

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { getSession } from 'next-auth/client';
 import Login from '../components/Login';
 import Sidebar from '../components/Sidebar';
 import Feed from '../components/Feed';
+import Widgets from '../components/Widgets';
 
 export default function Home({ session }) {
  if (!session) return <Login />;
@@ -19,7 +20,7 @@ export default function Home({ session }) {
       <main className="flex">
         <Sidebar />
         <Feed />
-        {/* Widgets */}
+        <Widgets />
       </main>
     </div>
   );
